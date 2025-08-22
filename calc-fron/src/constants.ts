79fb659e174e25dd9271e53cb6b16e742810636c
aniket2040 <aniket8873kumar@gmail.com>
2025-08-22 13:56:32 +0530

    Initial commit

---
+++ calc-fron/src/constants.ts
@@ -0,0 +1,15 @@
+const SWATCHES = [
+    "#ffffff",
+    "#ee3333",
+    "#e64980",
+    "#be4bdb",
+    "#839200",
+    "#228be6",
+    "#3333ee",
+    "#40c057",
+    "#fab005",
+    "#00aa00",
+    "#fd7e14",
+
+];
+export {SWATCHES};
